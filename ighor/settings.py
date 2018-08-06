01a2109a89e62cfe0bd488937407f73cc7c2d526
Ruben Gocio <rubengocio@gmail.com>
2018-08-06 13:13:13 -0300

    cambios

--- ighor/settings.py
+++ ighor/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'normalizador',
     'rest_framework',
     'rest_framework_swagger',
-    'corsheaders',
     'user',
 ]
 
