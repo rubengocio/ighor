2512a2d32bc5f8f0aaeb91ec47c0ebb03f184441
Ruben Gocio <rubengocio@gmail.com>
2018-11-03 17:11:02 -0300

    cambios

--- normalizador/models/calle_incorrecta.py
+++ normalizador/models/calle_incorrecta.py
@@ -8,12 +8,12 @@ class CalleIncorrecta(models.Model):
     @staticmethod
     def actualizar_calle_incorrecta(barrio_id):
         try:
-            query = ' INSERT INTO "normalizador_calleincorrecta"("nombre") '
-            query += ' SELECT "contacto_titular"."domicilio_calle" '
-            query += ' FROM "contacto_titular" '
-            query += ' INNER JOIN "normalizador_diccionariobarrio" ON ("normalizador_diccionariobarrio"."barrio_id"=%s AND "normalizador_diccionariobarrio"."nombre"="contacto_titular"."domicilio_barrio") '
-            query += ' WHERE NOT EXISTS (SELECT 1 FROM "normalizador_calleincorrecta" WHERE "normalizador_calleincorrecta"."nombre"="contacto_titular"."domicilio_calle") '
-            query += ' GROUP BY "contacto_titular"."domicilio_calle" '
+            query = ' INSERT INTO normalizador_calleincorrecta(nombre) '
+            query += ' SELECT contacto_titular.domicilio_calle '
+            query += ' FROM contacto_titular '
+            query += ' INNER JOIN normalizador_diccionariobarrio ON (normalizador_diccionariobarrio.barrio_id=%s AND normalizador_diccionariobarrio.nombre=contacto_titular.domicilio_barrio) '
+            query += ' WHERE NOT EXISTS (SELECT 1 FROM normalizador_calleincorrecta WHERE normalizador_calleincorrecta.nombre=contacto_titular.domicilio_calle) '
+            query += ' GROUP BY contacto_titular.domicilio_calle '
             query = query % str(barrio_id)
             cursor = connection.cursor()
             cursor.execute(query)
