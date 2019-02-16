12ddab5d7dd0d840a984490885d4f95afa58839e
Ruben Gocio <rubengocio@gmail.com>
2019-02-16 16:16:54 -0300

    cambios

---
+++ contacto/management/commands/dump_database.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        import os
+        import time
+
+        config = settings.DATABASES
+
+        default = config['default']
+
+        self.stdout.write(u"Buscando configuracion...")
+
+        if default.get('HOST', None):
+            self.stdout.write(u"Produccion")
+
+            NAME = default.get('NAME')
+            USER = default.get('USER')
+            PASSWORD = default.get('PASSWORD')
+            BACKUP_DIR = settings.BASE_DIR + '/backup'
+
+            datetime = time.strftime('%m%d%Y-%H%M%S')
+            datetimeBackupDir = BACKUP_DIR + datetime
+
+            self.stdout.write(u"Creando directorio")
+            if not os.path.exists(datetimeBackupDir):
+                os.makedirs(datetimeBackupDir)
+
+            mysqldump_cmd = "mysqldump -u " + USER + " --password='" + PASSWORD + "' -h mysql.server --databases '" + NAME + "' > " + datetimeBackupDir + "/" + NAME + ".sql"
+            os.system(mysqldump_cmd)
+            self.stdout.write(u"fin dump..")
+
+        self.stdout.write(u"Listo!")
